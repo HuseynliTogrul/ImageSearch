1cd00718602b181f118c8abffb213e87fff482b8
HuseynliTogrul <toghrulih@code.edu.az>
2024-10-15 13:58:01 +0400

    image search end

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,16 +1,21 @@
 import React from "react";
+import "../css/style.css";
 
 const ImageList = (props) => {
-    const images = props.images.map(image => {
-        return <img key={image.id} src={image.webformatURL} />
-    })
-
+  const images = props.images.map((image) => {
     return (
-        <div>
-            {images}
-        </div>
-    )
-}
+      <>
+        <img
+          className="img"
+          key={image.id}
+          src={image.webformatURL}
+          alt="img"
+        />
+      </>
+    );
+  });
 
+  return <div className="imgs">{images}</div>;
+};
 
 export default ImageList;
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,41 +1,38 @@
 import React from "react";
 
 class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { entry: "" };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { entry: '' }
-    }
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    if (!this.state.entry.trim()) return;
+    
+    this.props.onSearchSubmit(this.state.entry);
+  };
 
-    // onInputChange(event) {
-    //     this.setState(event.target.value)
-    // }
-
-    onFormSubmit = (event) => {
-        event.preventDefault();
-        this.props.onSearchSubmit(this.state.entry);
-    }
-
-    render() {
-        return (
-            // <div className="ui segment">
-            <form onSubmit={this.onFormSubmit} className="ui form">
-                <div className="field">
-                    <div className="ui massive icon input">
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            // onChange={this.onInputChange}
-                            onChange={(event) => this.setState({ entry: event.target.value })}
-                            value={this.state.entry}
-                        />
-                        <i className="search icon"></i>
-                    </div>
-                </div>
-            </form>
-            // </div>
-        )
-    }
+  render() {
+    return (
+      <form
+        onSubmit={this.onFormSubmit}
+        className="ui form"
+      >
+        <div className="field">
+          <div className="ui massive icon input">
+            <input
+              type="text"
+              placeholder="Search..."
+              onChange={(event) => this.setState({ entry: event.target.value })}
+              value={this.state.entry}
+            />
+            <i className="search icon"></i>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default SearchInput;
