e79d45a1c000ac182c6993549b6daca1f3eee82f
HuseynliTogrul <toghrulih@code.edu.az>
2025-01-31 11:56:43 +0400

    fix: edit style project

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,32 @@
 import React from "react";
-import axios from "axios"
+import axios from "axios";
 import SearchInput from "./SearchInput";
 import ImageList from "./ImageList";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { images: [] }
-    }
+  onSearchSubmit = async (entry) => {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=45225583-3ce0d5034080b713f21092676&q=${entry}&image_type=photo`
+    );
+    this.setState({ images: response.data.hits });
+  };
 
-    onSearchSubmit = async (entry) => {
-        const response = await axios.get(`https://pixabay.com/api/?key=45225583-3ce0d5034080b713f21092676&q=${entry}&image_type=photo`)
-        this.setState({ images: response.data.hits })
-    }
-
-    render() {
-        return (
-            <div className="ui container" style={{ marginTop: '30px' }}>
-                <SearchInput onSearchSubmit={this.onSearchSubmit} />
-                <ImageList images = {this.state.images}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        className="ui container"
+        style={{ paddingTop: "30px" }}
+      >
+        <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
+      </div>
+    );
+  }
 }
 
 export default App;
